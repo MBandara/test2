c5b55bc909f22d6ebe5113ddc11fcfdff22f84cd
MBandara <mbandara@mitrai.com>
2017-03-30 11:36:17 +0530

    intial commit

---
+++ PH_TestApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PH_TestApp.Startup))]
+namespace PH_TestApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
